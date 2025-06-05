f04353555edbad21c85c71a8785efbb27a832eba
Simon Legner <Simon.Legner@gmail.com>
2025-06-05 11:41:26 +0200

    vite library mode
    https://vite.dev/guide/build.html#library-mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  build: { sourcemap: true },
+  build: {
+    lib: {
+      entry: "/src/main.ts",
+      formats: ["es"],
+    },
+    sourcemap: true,
+  },
 });
