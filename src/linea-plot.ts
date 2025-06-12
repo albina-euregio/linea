e790d9e0aa8cfcba605fd1c7928ccbc7df4c93e2
Simon Legner <Simon.Legner@gmail.com>
2025-06-12 06:02:33 +0200

    showSurfaceHoarButton attribute

--- src/linea-plot.ts
+++ src/linea-plot.ts
@@ -76,21 +76,23 @@ export class LineaPlot extends HTMLElement {
       this.#addSeries(p, opts_TD, TD);
       this.#addSeries(p, opts_TSS, values.TSS);
 
-      const button = document.createElement("button");
-      button.classList.add("toggle-btn");
-      button.innerText = i18n.message(
-        "dialog:weather-station-diagram:parameter:SH:hide"
-      );
-      controls.append(button);
-      button.onclick = () => {
-        showSurfaceHoar.value = !showSurfaceHoar.value;
+      if (this.hasAttribute("showSurfaceHoarButton")) {
+        const button = document.createElement("button");
+        button.classList.add("toggle-btn");
         button.innerText = i18n.message(
-          showSurfaceHoar.value
-            ? "dialog:weather-station-diagram:parameter:SH:hide"
-            : "dialog:weather-station-diagram:parameter:SH:show"
+          "dialog:weather-station-diagram:parameter:SH:hide"
         );
-        p.redraw();
-      };
+        controls.append(button);
+        button.onclick = () => {
+          showSurfaceHoar.value = !showSurfaceHoar.value;
+          button.innerText = i18n.message(
+            showSurfaceHoar.value
+              ? "dialog:weather-station-diagram:parameter:SH:hide"
+              : "dialog:weather-station-diagram:parameter:SH:show"
+          );
+          p.redraw();
+        };
+      }
     }
 
     if (values.VW && values.DW) {
