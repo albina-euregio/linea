1c346975db526d6687647554f000e9fd3eb48878
leozoltan <leo.zol@icloud.com>
2025-07-24 10:32:05 +0000

    change font size of horizontal label

--- src/linea-plot/opts_RH_GR.ts
+++ src/linea-plot/opts_RH_GR.ts
@@ -24,7 +24,7 @@ export const opts_RH_GR: uPlot.Options = {
       (u) => {
                 const ctx = u.ctx;
                 ctx.save();
-                ctx.font = "bold 1.4vw sans-serif";
+                ctx.font = "bold 1.2vw sans-serif";
                 ctx.textAlign = "center";
                 ctx.textBaseline = "bottom";
                         
