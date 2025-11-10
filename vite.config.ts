9c3fe51a772c825722ea2960701142dc85b2e361
fynn_at_jackcachpot_de <fynn@jackcachpot.de>
2025-11-10 15:02:51 +0100

    Changed configuration to work on Fynns PC

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
+  server: {
+	  host: true,
+	  port: 5173,
+  },
   build: {
     lib: {
-      entry: "/src/main.ts",
+      entry: "./src/main.ts",
       formats: ["es"],
     },
     sourcemap: true,
