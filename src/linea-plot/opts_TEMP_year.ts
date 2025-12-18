c5066cf89104889bbef925fa2d0896c09c4280c0
fynn_at_jackcachpot_de <fynn@jackcachpot.de>
2025-12-18 08:44:32 +0100

    added plugin to axes

--- src/linea-plot/opts_TEMP_year.ts
+++ src/linea-plot/opts_TEMP_year.ts
@@ -3,6 +3,7 @@ import { cursorOpts } from "./cursorOpts";
 import { timeAxis } from "./timeAxisOpts";
 import { i18n } from "../i18n";
 import { OptsHelper } from "./optsHelper";
+import { TouchZoom } from "./touchZoom";
 
 /**
  * uPlot options for snow-height/year [cm]
@@ -22,6 +23,8 @@ export const opts_TEMP_year: uPlot.Options = {
       fill: (u, seriesIdx) => u.series[seriesIdx].stroke(u, seriesIdx),
     },
   },
+
+  plugins: [TouchZoom.touchZoomPlugin({})],
 
   hooks: {
     drawAxes: [
