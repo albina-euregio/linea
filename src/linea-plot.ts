d33c57dfdc1b7bfaac21868b478619a10247e85f
fynn_at_jackcachpot_de <fynn@jackcachpot.de>
2025-12-30 08:40:15 +0100

    added occitlan

--- src/linea-plot.ts
+++ src/linea-plot.ts
@@ -757,7 +757,18 @@ export class LineaPlot extends HTMLElement {
         locale = localeIt; // Italian
         break;
       case "oc":
-        locale = localeEn; // Occitan, not implemented yet
+        locale = {
+          days: ['Dimenge', 'Diluns', 'Mars', 'Dimècres', 'Dijòus', 'Divendres', 'Dissabte'],
+          daysShort: ['Dg', 'Dl', 'Dm', 'Dc', 'Dj', 'Dv', 'Ds'],
+          daysMin: ['Dg', 'Dl', 'Dm', 'Dc', 'Dj', 'Dv', 'Ds'],
+          months: ['Genièr', 'Febrièr', 'Març', 'Abril', 'Mai', 'Junh', 'Julhet', 'Agost', 'Setembre', 'Octòbre', 'Novembre', 'Decembre'],
+          monthsShort: ['Gen.', 'Feb.', 'Març', 'Abr.', 'Mai', 'Junh', 'Jul.', 'Ago.', 'Set', 'Oct', 'Nov', 'Dec'],
+          today: 'Uèi',
+          clear: 'Esfacar',
+          dateFormat: 'dd/mm/yyyy',
+          timeFormat: 'hh:ii',
+          firstDay: 1
+        };
         break;
       case "pl":
         locale = localePl; // Polish
