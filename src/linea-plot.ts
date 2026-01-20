616515a3e3b9f0427b25688ebf4e3663224a1e10
fynn_at_jackcachpot_de <fynn@jackcachpot.de>
2026-01-20 12:12:24 +0100

    refac(css): controls

--- src/linea-plot.ts
+++ src/linea-plot.ts
@@ -107,9 +107,6 @@ export class LineaPlot extends HTMLElement {
           display: flex;
           justify-content: center;
           align-items: center;
-          padding: 10px;
-          border-radius: 6px;
-          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         }
         
         linea-plot:focus {
@@ -184,6 +181,9 @@ export class LineaPlot extends HTMLElement {
           text-align: center;
           row-gap: 6px;
           column-gap: 6px;
+          padding: 20px;
+          border-radius: 6px;
+          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         }
         
         .controls-dates {
