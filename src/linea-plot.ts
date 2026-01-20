715e596c64f93a748dc0860f3fcb6c9767f3b7d0
fynn_at_jackcachpot_de <fynn@jackcachpot.de>
2026-01-20 16:38:37 +0100

    fix(css): removed border around controls

--- src/linea-plot.ts
+++ src/linea-plot.ts
@@ -181,9 +181,6 @@ export class LineaPlot extends HTMLElement {
           text-align: center;
           row-gap: 6px;
           column-gap: 6px;
-          padding: 20px;
-          border-radius: 6px;
-          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
         }
         
         .controls-dates {
