a21ecdb841fe4408f0d7f3068b2636572a5796dd
fynn_at_jackcachpot_de <fynn@jackcachpot.de>
2026-03-20 08:35:30 +0100

    chore(awsstats.vite.config.ts): created awsstats vite config

---
+++ awsstats.vite.config.ts
@@ -0,0 +1,59 @@
+import { defineConfig, type Plugin } from "vite";
+import * as z from "zod";
+import { FeatureCollectionSchema as LegacyFeatureCollectionSchema } from "./src/schema/listing-legacy";
+import { FeatureCollectionSchema } from "./src/schema/listing";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "./aws-stats/",
+  build: {
+    chunkSizeWarningLimit: 190,
+    license: { fileName: "license.json" },
+    lib: {
+      entry: "./src/aws-stats/main.ts",
+      formats: ["es", "cjs"],
+      name: "@albina-euregio/awsstats",
+    },
+    sourcemap: true,
+    rolldownOptions: {
+      output: {
+        advancedChunks: {
+          groups: [
+            {
+              test: /node_modules\/temporal-polyfill/,
+              name: "temporal-polyfill",
+            },
+            {
+              test: /node_modules\/uplot/,
+              name: "uplot",
+            },
+          ],
+        },
+      },
+    },
+  },
+  plugins: [
+    zodToJsonSchemaPlugin(FeatureCollectionSchema, "listing.schema.json"),
+    zodToJsonSchemaPlugin(LegacyFeatureCollectionSchema, "listing-legacy.schema.json"),
+  ],
+});
+
+function zodToJsonSchemaPlugin(type: z.ZodType, fileName: string): Plugin {
+  return {
+    name: "zod-to-json-schema",
+    apply: "build",
+    buildStart() {
+      const schema = type.toJSONSchema({
+        unrepresentable: "any",
+        override: ({ zodSchema, jsonSchema }) => {
+          if (zodSchema._zod.def.type === "date") {
+            jsonSchema.type = "string";
+            jsonSchema.format = "date-time";
+          }
+        },
+      });
+      const schemaJson = JSON.stringify(schema, undefined, 2);
+      this.emitFile({ type: "asset", fileName, source: schemaJson });
+    },
+  };
+}
