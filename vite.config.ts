06b83bc7660ae4e29699de9aa56b817ae13e6c20
fynn_at_jackcachpot_de <fynn@jackcachpot.de>
2026-03-24 11:21:55 +0100

    chore(vite:rolldown): changed deprecated advancedChunks to codeSplitting

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     sourcemap: true,
     rolldownOptions: {
       output: {
-        advancedChunks: {
+        codeSplitting: {
           groups: [
             {
               test: /node_modules\/temporal-polyfill/,
