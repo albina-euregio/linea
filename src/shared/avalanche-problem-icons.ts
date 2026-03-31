9f0b4a46518446a0e06aef4212815fe11dc15c20
fynn_at_jackcachpot_de <fynn@jackcachpot.de>
2026-03-31 15:35:19 +0200

    feat(avalanche-problem-icons): added icons to avalanche-problem-micro-regions-chart

---
+++ src/shared/avalanche-problem-icons.ts
@@ -0,0 +1,17 @@
+import icon5 from "../assets/Icon-Avalanche-Problem-Gliding-Snow-EAWS.svg";
+import icon2 from "../assets/Icon-Avalanche-Problem-New-Snow-EAWS.svg";
+import icon1 from "../assets/Icon-Avalanche-Problem-Persistent-Weak-Layer-EAWS.svg";
+import icon4 from "../assets/Icon-Avalanche-Problem-Wet-Snow-EAWS.svg";
+import icon3 from "../assets/Icon-Avalanche-Problem-Wind-Slab-EAWS.svg";
+
+const iconUrls = [icon1, icon2, icon3, icon4, icon5];
+export const LOADED_AVALANCHE_PROBLEM_ICONS = new Map<number, HTMLImageElement>();
+
+function preloadIcons() {
+  iconUrls.forEach((url, index) => {
+    const img = new Image();
+    img.src = url;
+    LOADED_AVALANCHE_PROBLEM_ICONS.set(index + 1, img);
+  });
+}
+preloadIcons();
