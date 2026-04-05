0a2212dd38d70be6fd67df1558a7fd8f94ffac62
Simon Legner <Simon.Legner@gmail.com>
2026-04-05 09:22:31 +0200

    chore: index-nav-buttons based on linea.geojson

---
+++ index-nav-buttons.ts
@@ -0,0 +1,17 @@
+import { fetchAll } from "./src/data/listing";
+
+const features = await fetchAll((c) => c.regions.includes("AT-07"));
+features.sort((f1, f2) => f1.properties.name.localeCompare(f2.properties.name));
+features.forEach((feature) => {
+  const button = document.createElement("button");
+  button.innerText = feature.properties.shortName ?? feature.properties.name.slice(0, 8);
+  button.title = feature.properties.name;
+  button.onclick = () => {
+    Array.from(document.getElementsByTagName("linea-plot")).forEach((plot, i) => {
+      if (i > 0) plot.remove();
+      plot.setAttribute("src", feature.$smet[0]);
+      plot.setAttribute("lazysrc", feature.$smet[1]);
+    });
+  };
+  document.querySelector("nav")!.append(button);
+});
