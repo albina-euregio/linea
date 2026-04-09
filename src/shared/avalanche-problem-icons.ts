1ba1c289bc1a28b66c7bb7ad78d89708f61768d0
Fynn Renner <fynn@jackcachpot.de>
2026-04-09 10:42:09 +0200

    refac(avalanche-problem-icons): make it good

--- src/shared/avalanche-problem-icons.ts
+++ src/shared/avalanche-problem-icons.ts
@@ -1,29 +1,25 @@
-import icon1 from "../assets/Icon-Avalanche-Problem-Persistent-Weak-Layer-EAWS.svg";
-import icon2 from "../assets/Icon-Avalanche-Problem-New-Snow-EAWS.svg";
-import icon3 from "../assets/Icon-Avalanche-Problem-Wind-Slab-EAWS.svg";
-import icon4 from "../assets/Icon-Avalanche-Problem-Wet-Snow-EAWS.svg";
-import icon5 from "../assets/Icon-Avalanche-Problem-Gliding-Snow-EAWS.svg";
-import icon6 from "../assets/Icon-Avalanche-Problem-Cornices.svg";
-import icon7 from "../assets/Icon-Avalanche-Problem-No-Distinct-Avalanche-Problem-EAWS.svg";
-import type { AvalancheProblemType } from "../schema/caaml";
+import persistent_weak_layers from "../assets/Icon-Avalanche-Problem-Persistent-Weak-Layer-EAWS.svg";
+import new_snow from "../assets/Icon-Avalanche-Problem-New-Snow-EAWS.svg";
+import wind_slab from "../assets/Icon-Avalanche-Problem-Wind-Slab-EAWS.svg";
+import wet_snow from "../assets/Icon-Avalanche-Problem-Wet-Snow-EAWS.svg";
+import gliding_snow from "../assets/Icon-Avalanche-Problem-Gliding-Snow-EAWS.svg";
+import cornices from "../assets/Icon-Avalanche-Problem-Cornices.svg";
+import no_distinct_avalanche_problem from "../assets/Icon-Avalanche-Problem-No-Distinct-Avalanche-Problem-EAWS.svg";
+import { type AvalancheProblemType } from "../schema/caaml";
 
-const avalancheProblemTypes: AvalancheProblemType[] = [
-  "persistent_weak_layers",
-  "new_snow",
-  "wind_slab",
-  "wet_snow",
-  "gliding_snow",
-  "cornices",
-  "no_distinct_avalanche_problem",
-];
-const iconUrls = [icon1, icon2, icon3, icon4, icon5, icon6, icon7];
-export const LOADED_AVALANCHE_PROBLEM_ICONS = new Map<AvalancheProblemType, HTMLImageElement>();
+export const LOADED_AVALANCHE_PROBLEM_ICONS = new Map<AvalancheProblemType, HTMLImageElement>([
+  ["persistent_weak_layers", img(persistent_weak_layers)],
+  ["new_snow", img(new_snow)],
+  ["wind_slab", img(wind_slab)],
+  ["wet_snow", img(wet_snow)],
+  ["gliding_snow", img(gliding_snow)],
+  ["cornices", img(cornices)],
+  ["no_distinct_avalanche_problem", img(no_distinct_avalanche_problem)],
+  ["new_snow", img(new_snow)],
+]);
 
-function preloadIcons() {
-  iconUrls.forEach((url, index) => {
-    const img = new Image();
-    img.src = url;
-    LOADED_AVALANCHE_PROBLEM_ICONS.set(avalancheProblemTypes[index], img);
-  });
+function img(src: string) {
+  const img = new Image();
+  img.src = src;
+  return img;
 }
-preloadIcons();
