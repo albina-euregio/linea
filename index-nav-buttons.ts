e768730b7baf3c79566aa3e306c7487602aa6e98
Simon Legner <Simon.Legner@gmail.com>
2026-04-16 20:36:09 +0200

    chore: update index.html

--- index-nav-buttons.ts
+++ index-nav-buttons.ts
@@ -1,6 +1,8 @@
-import { fetchAll } from "./src/data/fetch-listing";
+import { PROVIDERS } from "./src/data/providers";
 
-const features = await fetchAll((c) => c.regions.includes("AT-07"));
+const { features } = await PROVIDERS.filtered("", (c) =>
+  c.regions.includes("AT-07"),
+).fetchStationListing();
 features.sort((f1, f2) => f1.properties.name.localeCompare(f2.properties.name));
 features.forEach((feature) => {
   const button = document.createElement("button");
